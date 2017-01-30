3f4f2295dd8f2a1ebfbb1edc59112b2ba4a9c752
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-01-31 00:33:39 +0100

    Import headers and http modules to hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = '/api/heroes';
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) {}
+
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
