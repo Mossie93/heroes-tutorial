b92d7e5e718e276612aeacf4d4bc4192f46a31e0
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-01-30 15:58:43 +0100

    Export hero methods to heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -8,23 +6,6 @@ import { HeroService } from './hero.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
-
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+export class AppComponent {
+  title = "Tour of Horoes";
 }
