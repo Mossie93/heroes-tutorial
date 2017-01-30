e476ee953bfc0f33a95201ca7126fef74959c07e
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-01-31 00:35:15 +0100

    Add delete method to HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -42,4 +42,13 @@ export class HeroService {
       .then(res => res.json().data)
       .catch(this.handleError);
   }
+
+  delete(id: Number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http
+      .delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 }
